d2c01b1e0f5ac3d7729d033a4ff042b0afd3bce0
Aleksey <meysonsib200189@mail.ru>
2024-01-09 17:12:49 +0700

    урок 1 - разделил по пакетам согластно чистой архитектуры и принципа S - solid

---
+++ app/src/main/java/com/bignerdranch/android/shoppinglist/domain/DeleteShopItemUseCase.kt
@@ -0,0 +1,8 @@
+package com.bignerdranch.android.shoppinglist.domain
+
+class DeleteShopItemUseCase {
+
+    fun deleteShopItem(shopItem: ShopItem) {
+
+    }
+}
---
+++ app/src/main/java/com/bignerdranch/android/shoppinglist/domain/EditShopItemUseCase.kt
@@ -0,0 +1,8 @@
+package com.bignerdranch.android.shoppinglist.domain
+
+class EditShopItemUseCase {
+
+    fun editShopItem(shopItem: ShopItem) {
+
+    }
+}
