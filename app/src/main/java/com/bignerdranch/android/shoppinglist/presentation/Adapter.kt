934aff2479c71e9a01922a7215b0b4448350c24b
Aleksey <meysonsib200189@mail.ru>
2024-01-15 18:24:24 +0700

    урок 8 - убрал реализацию списка LinearLayout, доделал Adapter и инициализировал его в MainActivity, обавил в сеттер списка shopList метод - notifyDataSetChanged(), попробовал поменять названия в карточках элементов на активные и неактивные с изменением цвета карточки но столкнулся с багом при прокрутки списка rv и активные и неактивные элементы становятся одного красного цвета - решение этого бага в следующем коммите

--- app/src/main/java/com/bignerdranch/android/shoppinglist/presentation/Adapter.kt
+++ app/src/main/java/com/bignerdranch/android/shoppinglist/presentation/Adapter.kt
@@ -4,13 +4,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.shoppinglist.R
 import com.bignerdranch.android.shoppinglist.domain.ShopItem
 
 class Adapter : RecyclerView.Adapter<Adapter.ShopItemViewHolder>() {
 
-    private val list = listOf<ShopItem>()
+    var shopList = listOf<ShopItem>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
 
     class ShopItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val tvName: TextView = view.findViewById<TextView>(R.id.tv_name)
@@ -18,20 +23,37 @@ class Adapter : RecyclerView.Adapter<Adapter.ShopItemViewHolder>() {
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_shop_disabled, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(
+            R.layout.item_shop_disabled,
+            parent,
+            false
+        )
         return ShopItemViewHolder(view)
     }
 
     override fun onBindViewHolder(shopItemViewHolder: ShopItemViewHolder, position: Int) {
-        val shopItem = list[position]
-        shopItemViewHolder.tvName.text = shopItem.name
+        val shopItem = shopList[position]
+        val status = if (shopItem.enabled) {
+            "Active"
+        } else {
+            "Not Active"
+        }
+        shopItemViewHolder.tvName.text = "${shopItem.name} $status"
         shopItemViewHolder.tvCount.text = shopItem.count.toString()
-        shopItemViewHolder.itemView.setOnLongClickListener{
+        shopItemViewHolder.itemView.setOnLongClickListener {
             true
+        }
+        if (shopItem.enabled) {
+            shopItemViewHolder.tvName.setTextColor(
+                ContextCompat.getColor(
+                    shopItemViewHolder.itemView.context,
+                    android.R.color.holo_red_light
+                )
+            )
         }
     }
 
     override fun getItemCount(): Int {
-        return list.size
+        return shopList.size
     }
 }
--- app/src/main/java/com/bignerdranch/android/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/shoppinglist/presentation/MainActivity.kt
@@ -6,48 +6,29 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.shoppinglist.R
 import com.bignerdranch.android.shoppinglist.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
-    private lateinit var linearLayoutShopList: LinearLayout
+    private lateinit var adapter: Adapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        linearLayoutShopList = findViewById(R.id.ll_shop_list)
 
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
-        viewModel.shopList.observe(this)
-        {
-            showLinearLayoutList(it)
+        viewModel.shopList.observe(this) {
+            adapter.shopList = it
         }
-
+        initRecyclerView()
     }
 
-    private fun showLinearLayoutList(list: List<ShopItem>) {
-        linearLayoutShopList.removeAllViews()
-        //помещаю cardView в view макета
-        for (item in list) {
-            val layoutId = if (item.enabled) {
-                R.layout.item_shop_enabbled
-            } else {
-                R.layout.item_shop_disabled
-            }
-            val view = LayoutInflater.from(this).inflate(layoutId, linearLayoutShopList, false)
-            //нахожу в макете вьюшки
-            val tvName = view.findViewById<TextView>(R.id.tv_name)
-            val tvCount = view.findViewById<TextView>(R.id.tv_count)
-            tvName.text = item.name
-            tvCount.text = item.count.toString()
-            view.setOnLongClickListener{
-                viewModel.changeEnableState(item)
-                true
-            }
-            //добавляю этот готовый view в макет
-            linearLayoutShopList.addView(view)
-        }
+    private fun initRecyclerView() {
+        val recyclerViewShopList = findViewById<RecyclerView>(R.id.rv_shop_list)
+        adapter = Adapter()
+        recyclerViewShopList.adapter = adapter
     }
 }
