2b8e8c434ec7565151615a581d262dd9e94f589b
Aleksey <meysonsib200189@mail.ru>
2024-01-22 20:21:15 +0700

    урок 16 - инициализировал views экрана редактирования и добавил fun проверки intent переданных параметров,добавил новые const val

--- app/src/main/java/com/bignerdranch/android/shoppinglist/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/bignerdranch/android/shoppinglist/presentation/ShopItemActivity.kt
@@ -4,16 +4,64 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.bignerdranch.android.shoppinglist.R
+import com.bignerdranch.android.shoppinglist.domain.ShopItem
+import com.google.android.material.textfield.TextInputEditText
 
 class ShopItemActivity : AppCompatActivity() {
+
+    private lateinit var shopItemViewModel: ShopItemViewModel
+
+    private lateinit var tilName: TextInputEditText
+    private lateinit var tilCount: TextInputEditText
+    private lateinit var editText: EditText
+    private lateinit var editCount: EditText
+    private lateinit var buttonSave: Button
+
+    private var screenMode = MODE_UNKNOWN
+    private var shopItemId = ShopItem.UNDEFINED_ID
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
+        parseIntent()
 
+        shopItemViewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
+
+        initViews()
         receiveTheTransmittedMessage()
+    }
+
+    /**fun проверки правильно ли переданы параметры интента
+     *!intent.hasExtra(EXTRA_SCREEN_MODE - если интент не содержит парам .. то ошибка */
+    private fun parseIntent() {
+        if (!intent.hasExtra(EXTRA_SCREEN_MODE)) {
+            throw RuntimeException("параметр screen mode отсутствует")
+        }
+        val mode = intent.getStringExtra(EXTRA_SCREEN_MODE)
+        if (mode != MODE_EDIT && mode != MODE_ADD) {
+            throw RuntimeException("Unknown screen mode $mode")
+        }
+        screenMode = mode
+        /**делаем проверку чтобы в интенте при редактировании был передан параметр id*/
+        if (screenMode == MODE_EDIT && !intent.hasExtra(EXTRA_SHOP_ITEM_ID)) {
+            throw RuntimeException("параметр shop item id отсутствует")
+        }
+        if (screenMode == MODE_EDIT) {
+            shopItemId = intent.getIntExtra(EXTRA_SHOP_ITEM_ID, ShopItem.UNDEFINED_ID)
+        }
+    }
+
+    private fun initViews() {
+        tilName = findViewById(R.id.text_input_layout_name)
+        tilCount = findViewById(R.id.text_input_layout_count)
+        editText = findViewById(R.id.edit_text_name)
+        editCount = findViewById(R.id.edit_count)
+        buttonSave = findViewById(R.id.button_save)
     }
 
     private fun receiveTheTransmittedMessage() {
@@ -27,6 +75,8 @@ class ShopItemActivity : AppCompatActivity() {
         private const val EXTRA_SHOP_ITEM_ID = "extra_shop_item_id"
         private const val MODE_EDIT = "mode_edit"
         private const val MODE_ADD = "mode_add"
+
+        private const val MODE_UNKNOWN = ""
 
         fun newIntentAddItem(context: Context): Intent {
             val intent = Intent(context, ShopItemActivity::class.java)
